1cbf4ea95ceec9c281e7de176f59b59d28605ddc
kamszel <info@kamilszela.pl>
2018-02-12 20:11:21 +0100

    use statsCollector in FilterInterface

---
+++ src/TestCase/ResponseFilters/AbstractFilter.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Rest-Control package.
+ *
+ * (c) Kamil Szela <kamil.szela@cothe.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RestControl\TestCase\ResponseFilters;
+
+use RestControl\TestCase\StatsCollector\StatsCollector;
+use RestControl\TestCase\StatsCollector\StatsCollectorInterface;
+
+/**
+ * Class AbstractFilter
+ *
+ * @package RestControl\TestCase\ResponseFilters
+ */
+abstract class AbstractFilter
+{
+    /**
+     * @var StatsCollectorInterface|null
+     */
+    protected $statsCollector = null;
+
+    /**
+     * @param StatsCollectorInterface|null $statsCollector
+     */
+    public function setStatsCollection(StatsCollectorInterface $statsCollector = null)
+    {
+        $this->statsCollector = $statsCollector;
+    }
+
+    /**
+     * @return StatsCollectorInterface|null
+     */
+    public function getStatsCollector()
+    {
+        if($this->statsCollector) {
+           return $this->statsCollector;
+        }
+
+        $this->statsCollector = new StatsCollector();
+
+        return $this->statsCollector;
+    }
+}
