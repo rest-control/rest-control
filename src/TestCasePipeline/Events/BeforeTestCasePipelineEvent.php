f18f86a224550a5d5839dc5ebd0c63e19195ac63
kamszel <info@kamilszela.pl>
2018-02-13 22:51:39 +0100

    create before test case pipeline event

---
+++ src/TestCasePipeline/Events/BeforeTestCasePipelineEvent.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * This file is part of the Rest-Control package.
+ *
+ * (c) Kamil Szela <kamil.szela@cothe.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RestControl\TestCasePipeline\Events;
+
+use League\Pipeline\PipelineInterface;
+use RestControl\ApiClient\ApiClientInterface;
+use RestControl\Loader\TestsBag;
+use RestControl\TestCasePipeline\TestPipelineConfiguration;
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class BeforeTestCasePipelineEvent
+ *
+ * @package RestControl\TestCasePipeline\Events
+ */
+class BeforeTestCasePipelineEvent extends Event
+{
+    const NAME = 'before.testCasePipeline';
+
+    /**
+     * @var PipelineInterface
+     */
+    protected $pipeline;
+
+    /**
+     * @var TestsBag
+     */
+    protected $testsBag;
+
+    /**
+     * @var TestPipelineConfiguration
+     */
+    protected $configuration;
+
+    /**
+     * @var ApiClientInterface
+     */
+    protected $apiClient;
+
+    /**
+     * BeforeTestCasePipelineEvent constructor.
+     *
+     * @param PipelineInterface         $pipeline
+     * @param TestsBag                  $testsBag
+     * @param TestPipelineConfiguration $configuration
+     * @param ApiClientInterface        $apiClient
+     */
+    public function __construct(
+        PipelineInterface $pipeline,
+        TestsBag $testsBag,
+        TestPipelineConfiguration $configuration,
+        ApiClientInterface $apiClient
+    ) {
+        $this->pipeline      = $pipeline;
+        $this->testsBag      = $testsBag;
+        $this->configuration = $configuration;
+        $this->apiClient     = $apiClient;
+    }
+
+    /**
+     * @return PipelineInterface
+     */
+    public function getPipeline()
+    {
+        return $this->pipeline;
+    }
+
+    /**
+     * @return TestsBag
+     */
+    public function getTestsBag()
+    {
+        return $this->testsBag;
+    }
+
+    /**
+     * @return TestPipelineConfiguration
+     */
+    public function getConfiguration()
+    {
+        return $this->configuration;
+    }
+
+    /**
+     * @return ApiClientInterface
+     */
+    public function getApiClient()
+    {
+        return $this->apiClient;
+    }
+}
---
+++ tests/TestCasePipeline/Events/BeforeTestCasePipelineEventTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Rest-Control package.
+ *
+ * (c) Kamil Szela <kamil.szela@cothe.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RestControl\Tests\TestCasePipeline\Events;
+
+use League\Pipeline\PipelineInterface;
+use PHPUnit\Framework\TestCase;
+use RestControl\ApiClient\ApiClientInterface;
+use RestControl\Loader\TestsBag;
+use RestControl\TestCasePipeline\Events\BeforeTestCasePipelineEvent;
+use RestControl\TestCasePipeline\TestPipelineConfiguration;
+
+class BeforeTestCasePipelineEventTest extends TestCase
+{
+    public function testEvent()
+    {
+        $pipeline = $this->getMockBuilder(PipelineInterface::class)
+            ->getMockForAbstractClass();
+        $testsBag = $this->getMockBuilder(TestsBag::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $configuration = $this->getMockBuilder(TestPipelineConfiguration::class)
+            ->getMockForAbstractClass();
+        $apiClient = $this->getMockBuilder(ApiClientInterface::class)
+            ->getMockForAbstractClass();
+
+        $event = new BeforeTestCasePipelineEvent(
+            $pipeline,
+            $testsBag,
+            $configuration,
+            $apiClient
+        );
+
+        $this->assertSame('before.testCasePipeline', BeforeTestCasePipelineEvent::NAME);
+        $this->assertSame($pipeline, $event->getPipeline());
+        $this->assertSame($testsBag, $event->getTestsBag());
+        $this->assertSame($configuration, $event->getConfiguration());
+        $this->assertSame($apiClient, $event->getApiClient());
+    }
+}
+
