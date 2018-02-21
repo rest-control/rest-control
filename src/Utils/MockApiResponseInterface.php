9b40f08a98a665bce85ab44ff844d14bcf1ce1e4
kamszel <info@kamilszela.pl>
2018-02-21 21:38:26 +0100

    create definition of mock api response

---
+++ src/Utils/MockApiResponseInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Rest-Control package.
+ *
+ * (c) Kamil Szela <kamil.szela@cothe.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RestControl\Utils;
+
+use RestControl\ApiClient\ApiClientRequest;
+
+/**
+ * Interface MockApiResponseInterface
+ *
+ * @package RestControl\Utils
+ */
+interface MockApiResponseInterface
+{
+    /**
+     * Returns full endpoint url.
+     *
+     * e.q. 'get::http://sample.site/sample/endpoint/{id}'
+     *
+     * @return string
+     */
+    public function getUrl();
+
+    /**
+     * Transform $request into $apiClientResponse.
+     *
+     * @return \RestControl\ApiClient\ApiClientResponse
+     */
+    public function getApiClientResponse(ApiClientRequest $request);
+}
