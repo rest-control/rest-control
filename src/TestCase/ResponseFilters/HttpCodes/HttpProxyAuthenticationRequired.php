dfa05444490c8057df3c19c49696f3b909038839
kamszel <info@kamilszela.pl>
2018-03-01 21:23:21 +0100

    add few predefined http status codes filters

---
+++ src/TestCase/ResponseFilters/HttpCodes/HttpProxyAuthenticationRequired.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Rest-Control package.
+ *
+ * (c) Kamil Szela <kamil.szela@cothe.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RestControl\TestCase\ResponseFilters\HttpCodes;
+
+class HttpProxyAuthenticationRequired extends AbstractPredefinedHttpCode
+{
+    const FILTER_NAME      = 'httpStatusProxyAuthenticationRequired';
+    const HTTP_STATUS_CODE = 407;
+}
