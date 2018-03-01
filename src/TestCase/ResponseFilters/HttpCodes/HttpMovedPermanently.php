80228650e61575c0cd23a3f2d73071f6fc20afe4
kamszel <info@kamilszela.pl>
2018-03-01 20:46:44 +0100

    add few predefined http status codes filters

---
+++ src/TestCase/ResponseFilters/HttpCodes/HttpMovedPermanently.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Rest-Control package.
+ *
+ * (c) Kamil Szela <kamil.szela@cothe.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RestControl\TestCase\ResponseFilters\HttpCodes;
+
+class HttpMovedPermanently extends AbstractPredefinedHttpCode
+{
+    const FILTER_NAME      = 'httpStatusMovedPermanently';
+    const HTTP_STATUS_CODE = 301;
+}
