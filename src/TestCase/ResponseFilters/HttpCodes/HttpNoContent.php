50f87facaaecf91b125c3978c220ea0f9ca56ad9
kamszel <info@kamilszela.pl>
2018-03-01 20:46:53 +0100

    add few predefined http status codes filters

---
+++ src/TestCase/ResponseFilters/HttpCodes/HttpNoContent.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Rest-Control package.
+ *
+ * (c) Kamil Szela <kamil.szela@cothe.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RestControl\TestCase\ResponseFilters\HttpCodes;
+
+class HttpNoContent extends AbstractPredefinedHttpCode
+{
+    const FILTER_NAME      = 'httpStatusNoContent';
+    const HTTP_STATUS_CODE = 204;
+}
