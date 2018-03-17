ce719a860ada8ae4ec4c72d550c316ae3f7ee52d
kamszel <info@kamilszela.pl>
2018-03-17 13:29:00 +0100

    update testObject jsonSerializable

--- src/TestCasePipeline/TestObject.php
+++ src/TestCasePipeline/TestObject.php
@@ -207,6 +207,7 @@ class TestObject implements JsonSerializable
                 'request'       => $request,
                 'response'      => $response,
             ],
+            'statistics'    => $this->statsCollector,
         ];
     }
 }
