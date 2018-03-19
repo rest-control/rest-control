be206ab1f3fcfd7eb53342d501b920bed674464a
kamszel <info@kamilszela.pl>
2018-03-19 23:09:58 +0100

    add common content types filters

---
+++ src/TestCase/ResponseFilters/ContentTypes/AudioAac.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Rest-Control package.
+ *
+ * (c) Kamil Szela <kamil.szela@cothe.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RestControl\TestCase\ResponseFilters\ContentTypes;
+
+class AudioAac extends AbstractContentType
+{
+    const FILTER_NAME = 'contentTypeAudioAac';
+    const HTTP_CONTENT_TYPES  = ['audio/aac'];
+}
