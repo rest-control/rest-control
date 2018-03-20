a9c14cfca2d332de556caf4d18e66c3bd76f8062
kamszel <info@kamilszela.pl>
2018-03-20 20:51:05 +0100

    #30 [ResponseFilter] Custom filter

---
+++ tests/TestCase/ResponseFilters/CallFilterTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Rest-Control package.
+ *
+ * (c) Kamil Szela <kamil.szela@cothe.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RestControl\Tests\TestCase\ResponseFilters;
+
+use RestControl\TestCase\ResponseFilters\CallFilter;
+use PHPUnit\Framework\TestCase;
+
+class CallFilterTest extends TestCase
+{
+    public function testName()
+    {
+        $this->assertSame('call', (new CallFilter())->getName());
+    }
+
+    public function testValidateParams()
+    {
+        $filter = new CallFilter();
+
+        $this->assertFalse($filter->validateParams(['sample']));
+        $this->assertTrue($filter->validateParams([
+            function(){return true;}
+        ]));
+    }
+}
